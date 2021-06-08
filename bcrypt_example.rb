bb42e4e76d6888c81b42f937c891eae8c66940d7
Michael Matich <matich.mike@gmail.com>
2021-06-08 11:47:13 -0400

    bcrypt example

---
+++ bcrypt_example.rb
@@ -0,0 +1,8 @@
+require 'bcrypt'
+
+my_password = BCrypt::Password.create("my password")
+puts my_password
+puts my_password.version
+puts my_password.cost
+puts my_password == "my password"
+puts my_password == "not my password"
